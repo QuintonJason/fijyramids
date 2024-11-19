a79cd653c8a80c31b5d36a94a8ac01ad3eba2fe1
Quinton Jason <quinton.jason@gmail.com>
2024-11-19 10:21:45 -0600

    added slick carousel and updated photos

--- js/main.js
+++ js/main.js
@@ -1,6 +1,29 @@
+$(document).ready(function(){
+  $('.fijyramid-carousel').slick({
+		autoplay: true,
+		autoplaySpeed: 2000,
+		arrows: true,
+		fade: true,
+		dots: true
+  });
+});
+
+
+// $(document).ready(function(){
+// 	$('.work-slideshow').slick({
+//   dots: true,
+//   infinite: true,
+//   speed: 500,
+//   fade: true,
+// 	autoplay: true,
+//   cssEase: 'linear'
+// });
+// });
+
 const toggleButton = document.querySelector('.toggle-button');
 const navbar = document.querySelector('.navbar ul');
 
 toggleButton.addEventListener('click', () => {
     navbar.classList.toggle('active');
 });
+
